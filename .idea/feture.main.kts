74bde22a13376c058714dfa4278f24acde3fbe05
Daria <dashulya0445@gmail.com>
2025-10-06 19:28:20 +0300

    add new class

---
+++ .idea/feture.main.kts
@@ -0,0 +1,20 @@
+#!/usr/bin/env kotlin
+
+import android.os.Bundle
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
